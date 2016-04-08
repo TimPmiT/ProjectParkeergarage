00ac5ee981f3b4d6735ef83d5dba8930c5086bc4
Tim Petersen <t.petersen@st.hanze.nl>
2016-04-08 12:46:57 +0200

    Edit aan comment

--- ProjectParkeergarage/src/ParkingPassHolder.java
+++ ProjectParkeergarage/src/ParkingPassHolder.java
@@ -2,7 +2,7 @@
 public class ParkingPassHolder extends Car {
 
 	  
-	  //the parkingpass holder, this customer can swoowsh to the end of the que without playing locally
+	  //the parkingpass holder, this customer can swoowsh to the end of the queue without playing locally
 	  public ParkingPassHolder() {
 		  setIsMember(); 
 		  
