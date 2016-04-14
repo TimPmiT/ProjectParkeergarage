619064b7878dccd4c1b3a5a571dfc04d5eb229a6
Tim Petersen <t.petersen@st.hanze.nl>
2016-04-15 01:20:26 +0200

    Update to PieView and StatPieView

--- ProjectParkeergarage/src/view/PieView.java
+++ ProjectParkeergarage/src/view/PieView.java
@@ -21,6 +21,7 @@ public class PieView extends AbstractView {
 	private int y;
 	private int amountCars;
 	private int amountPassHolders;
+	private Image pieViewImage;
 	private static final int WIDTH_ARC = 200;
 	private static final int HEIGHT_ARC = 200;
 	private static final int MAX_DEGREES = 360;
@@ -36,7 +37,7 @@ public class PieView extends AbstractView {
     public PieView(CarParkLogic model) {
         super(model);
 
-        this.size = new Dimension(100, 100);
+        this.size = new Dimension(0, 0);
 
     	gradenHoekStart = 0;
     	x = 50;
@@ -46,52 +47,57 @@ public class PieView extends AbstractView {
 
     // TODO Volgens mij moet dit in de methode updateview maar dan heeft ie geen
     // g graphics meer om op te tekenen, sooooo, what to do
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
+    	if (pieViewImage == null) {
+    		return;
+    	}
+    	
+    	Dimension currentSize = getSize();
+    	if(size.equals(currentSize)) {
+    		g.drawImage(pieViewImage, 0, 0, null);
+    	} else {
+    		g.drawImage(pieViewImage,  0, 0 , currentSize.width, currentSize.height, null);
+    	}
+    }
+    
+    /**
+     * Get's called by the super model class when something needs to be updated.
+     */
+    public void updateView() {
+    	CarParkLogic carPark = (CarParkLogic) super.model;
+   		amountCars = carPark.getTotalCars();
+   		amountPassHolders = carPark.getTotalPassHolders();
+   		
+    	if (!size.equals(getSize())) {
+    		size = getSize();
+    		pieViewImage = createImage(size.width, size.height);
+    	}
+    	
+    	Graphics graphics = pieViewImage.getGraphics();
+    	
     	// Background of pie graph
-    	//g.setColor(Color.WHITE);
-    	//g.fillOval(x, y, WIDTH_ARC, HEIGHT_ARC);
+    	graphics.setColor(Color.WHITE);
+    	graphics.fillRect(x, y, WIDTH_ARC+10, HEIGHT_ARC+10);
+    	graphics.setColor(Color.LIGHT_GRAY);
+    	graphics.fillOval(x+5, y+5, WIDTH_ARC, HEIGHT_ARC);
+    	
     	
 		// Slice 1 \\
 		// methode van maken TODO 
 		int procent = (amountCars * PERCENT) / MAX_CARS;
 		int gradenHoek2 = ((procent * MAX_DEGREES) / PERCENT);
-		g.setColor(Color.RED);
-		g.fillArc(x, y, WIDTH_ARC, HEIGHT_ARC, gradenHoekStart, gradenHoek2);
+		graphics.setColor(Color.RED);
+		graphics.fillArc(x+5, y+5, WIDTH_ARC, HEIGHT_ARC, gradenHoekStart, gradenHoek2);
 		
 		// Slice 2 \\
 		int procent2 = ((amountPassHolders * PERCENT) / MAX_CARS);
 		int gradenHoek3 = ((procent2 * MAX_DEGREES) / PERCENT);
 		int start = gradenHoekStart + gradenHoek2;
-		g.setColor(Color.GREEN);
-		g.fillArc(x, y, WIDTH_ARC, HEIGHT_ARC, start, gradenHoek3);
-		
-    	/*
-		// Legenda vierkantjes \\
-		int xRect = 50;
-		int yRect = 250;
-		g.setColor(Color.RED);
-		g.fillRect(xRect, yRect, 10, 10);
-		
-		int xRect2 = 50;
-		int yRect2 = 270;
-		g.setColor(Color.GREEN);
-		g.fillRect(xRect2, yRect2, 10, 10);
-		*/
-		
-		
+		graphics.setColor(Color.GREEN);
+		graphics.fillArc(x+5, y+5, WIDTH_ARC, HEIGHT_ARC, start, gradenHoek3);
+    	
+		setVisible(true);
+	    super.updateView();
     }
     
-    /**
-     * Get's called by the super model class when something needs to be updated
-     */
-    public void updateView(){
-
-        CarParkLogic carPark = (CarParkLogic) super.model;
-
-    	amountCars = carPark.getTotalCars();
-    	amountPassHolders = carPark.getTotalPassHolders();
-
-        setVisible(true);
-        super.updateView();
-    }
 }
